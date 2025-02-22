1c7c87f36126c9ff33bcc0b9f8596449ff5751c1
Snehal <snehalsingh353@gmail.com>
2025-02-22 11:57:31 +0530

    DFS representation of the tree

---
+++ DFSTree.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        data = value;
+        left = right = NULL;
+    }
+};
+
+// DFS approach
+//   LEFT - ROOT - RIGHT
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// ROOT - LEFT - RIGHT
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// LEFT - ROOT - RIGHT
+void postorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "Post order representation" << endl;
+    postorder(root);
+
+    cout << "\nPre order representation" << endl;
+    preorder(root);
+
+    cout << "\n In order representation" << endl;
+    inorder(root);
+}
